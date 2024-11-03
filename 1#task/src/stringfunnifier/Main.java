f530db522fd778b06390d9c95d8ccbd343c0193b
khaled <khaledleader012@gmail.com>
2024-11-03 10:25:03 +0200

    task 2 incrase the fun

--- 1#task/src/stringfunnifier/Main.java
+++ 1#task/src/stringfunnifier/Main.java
@@ -2,11 +2,12 @@ package src.stringfunnifier;
 
 public class Main {
     public static void main(String[] args) {
-        String str = "abbccCDdBCCcc";
-        int[] startIndexes = {1, 5, 7, 11};
-        int[] endIndexes = {3, 5, 10, 12};
+        String str = "ccchHJKkklmlmmml";
+        int[] startIndexes = {1, 5, 7, 11, 13};
+        int[] endIndexes = {3, 5, 10, 12, 14};
+        String[] funOperations = {"REVERSE", "UPPERCASE", "SORT", "LOWERCASE", "COMPRESSION"};
 
-        StringFunifier funifier = new StringFunifier(str, startIndexes, endIndexes);
+        StringFunifier funifier = new StringFunifier(str, funOperations, startIndexes, endIndexes);
         String funnyString = funifier.getFunnyString();
 
         System.out.println(funnyString);
--- 1#task/src/stringfunnifier/StringFunifier.java
+++ 1#task/src/stringfunnifier/StringFunifier.java
@@ -1,14 +1,19 @@
 package src.stringfunnifier;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+
 public class StringFunifier {
     private String boringString;
     private int[] startIndexes;
     private int[] endIndexes;
+    private String[]  funOperations;
 
-    public StringFunifier(String str, int[] startIndexes, int[] endIndexes) {
-        this.boringString = str;
+    public StringFunifier(String boringString, String[]  funOperations, int[] startIndexes, int[] endIndexes) {
+        this.boringString = boringString;
         this.startIndexes = startIndexes;
         this.endIndexes = endIndexes;
+        this. funOperations =  funOperations;
     }
 
     public String getFunnyString() {
@@ -17,10 +22,41 @@ public class StringFunifier {
         for (int i = 0; i < startIndexes.length; i++) {
             int start = startIndexes[i];
             int end = endIndexes[i];
+            String opration = funOperations[i];
             result.append(boringString, lastEnd, start);
             String subStr = boringString.substring(start, end + 1);
-            StringBuilder revStr = new StringBuilder(new StringBuilder(subStr).reverse().toString());
-            result.append("(").append(revStr).append(")");
+            if (opration.equals("REVERSE")) {
+                result.append("(").append(new StringBuilder(subStr).reverse()).append(")");
+            }
+            if (opration.equals("UPPERCASE")) {
+                String UpperStr = new String(subStr).toUpperCase();
+                result.append("(").append(UpperStr).append(")");
+            }
+            if (opration.equals("SORT")) {
+                char[] ch = subStr.toCharArray();
+                for (int k = 0; k < ch.length; k++) {
+                    Arrays.sort(ch);
+                }
+                String sortedStr = new String(ch);
+                result.append("(").append(sortedStr).append(")");
+            }
+            if (opration.equals("LOWERCASE")) {
+                String UpperStr = new String(subStr).toLowerCase();
+                result.append("(").append(UpperStr).append(")");
+            }
+            if (opration.equals("COMPRESSION")) {
+                int repeted = 1;
+                for (int j = 0; j < subStr.length(); j++) {
+                    if (j < subStr.length() - 1) {
+                    if (subStr.charAt(j) == subStr.charAt(j+1)) {
+                        repeted++;
+                        }
+                    }
+                }
+                String copmartionStr = new String(subStr.charAt(0) + "");
+                result.append("(").append(copmartionStr).append(repeted).append(")");
+            }
+
             lastEnd = end + 1;
         }
         if (lastEnd < boringString.length()) {
