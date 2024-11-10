76ee5af2f49e1b355d79682ece61099adbb270ab
khaled <khaledleader012@gmail.com>
2024-11-10 13:09:32 +0200

    add more unit tests to string funnfier

--- 1#task/src/stringfunnifier/Main.java
+++ 1#task/src/stringfunnifier/Main.java
@@ -18,4 +18,16 @@ public class Main {
 
         System.out.println(funnyString);
     }
+    @Test
+    void doesGetFunnyWork() {
+        String boringString = "ccchHJKkklmlmmml";
+        String boringStringExpected = "c(hcc)H(J)K(kklm)(lm)(m1)l";
+        int[] startIndexes = {1, 5, 7, 11, 13};
+        int[] endIndexes = {3, 5, 10, 12, 14};
+        String[] funOperations = {"REVERSE", "UPPERCASE", "SORT", "LOWERCASE", "COMPRESSION"};
+
+        StringFunifier funifier = new StringFunifier(boringString, funOperations, startIndexes, endIndexes);
+        String funnyString = funifier.getFunnyString();
+        assertEquals(boringStringExpected, funnyString);
+    }
 }
--- 1#task/src/stringfunnifuertest/StringFunnifuerTest.java
+++ 1#task/src/stringfunnifuertest/StringFunnifuerTest.java
@@ -2,32 +2,46 @@ package src.stringfunnifuertest;
 
 import org.junit.jupiter.api.Test;
 import src.operation.*;
+import src.stringfunnifier.StringFunifier;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 public class StringFunnifuerTest {
     @Test
-    void doesCompareWork(){
+    void doesCompareWork() {
         Copmare testcompare = new Copmare();
         assertEquals("(s3)", testcompare.operate("sssl"));
     }
     @Test
-    void doesLowerWork(){
+    void doesLowerWork() {
         Lower testLower = new Lower();
         assertEquals("(l)", testLower.operate("l"));
     }
     @Test
-    void doesSortedWork(){
+    void doesSortedWork() {
         Sorted testSorted = new Sorted();
         assertEquals("(abcd)", testSorted.operate("cdba"));
     }
     @Test
-    void doesReversedWork(){
+    void doesReversedWork() {
         Reversed testReversed = new Reversed();
         assertEquals("(dcba)", testReversed.operate("abcd"));
     }
     @Test
-    void doesUpperWork(){
+    void doesUpperWork() {
         Upper testUpper = new Upper();
         assertEquals("(A)", testUpper.operate("a"));
+    }
+    @Test
+    void doesGetFunnyWork() {
+        String boringString = "ccchHJKkklmlmmml";
+        String boringStringExpected = "c(hcc)H(J)K(kklm)(lm)(m1)l";
+        int[] startIndexes = {1, 5, 7, 11, 13};
+        int[] endIndexes = {3, 5, 10, 12, 14};
+        String[] funOperations = {"REVERSE", "UPPERCASE", "SORT", "LOWERCASE", "COMPRESSION"};
+
+        StringFunifier funifier = new StringFunifier(boringString, funOperations, startIndexes, endIndexes);
+        String funnyString = funifier.getFunnyString();
+        assertEquals(boringStringExpected, funnyString);
     }
 }
